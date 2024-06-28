de5c48ca7be40491637e9cbb46f2fbb99e438f2d
AdrianCabreraPhi <adriancabproven@gmail.com>
2024-06-28 17:04:07 +0200

    define endpoint fields

--- src/app/define-endpoint/define-endpoint.component.ts
+++ src/app/define-endpoint/define-endpoint.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-define-endpoint',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './define-endpoint.component.html',
   styleUrl: './define-endpoint.component.scss'
 })
