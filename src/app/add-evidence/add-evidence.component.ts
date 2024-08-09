fa2e53e245221ab8b880c0311a389d052a4f2210
AdrianCabreraPhi <adriancabproven@gmail.com>
2024-08-09 12:19:22 +0200

    new fields added

--- src/app/add-evidence/add-evidence.component.ts
+++ src/app/add-evidence/add-evidence.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'app-add-evidence',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule,MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule],
   templateUrl: './add-evidence.component.html',
   styleUrl: './add-evidence.component.scss'
 })
 export class AddEvidenceComponent {
+  
+  constructor() {
+
+  }
 
 }
