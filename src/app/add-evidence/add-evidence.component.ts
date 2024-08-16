fb9e7bb913b21b44db09aeab2fabdacea56660d7
AdrianCabreraPhi <adriancabproven@gmail.com>
2024-08-16 12:49:34 +0200

    negative positive field added

--- src/app/add-evidence/add-evidence.component.ts
+++ src/app/add-evidence/add-evidence.component.ts
@@ -4,10 +4,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 @Component({
   selector: 'app-add-evidence',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule],
+  imports: [FormsModule,MatCheckboxModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule],
   templateUrl: './add-evidence.component.html',
   styleUrl: './add-evidence.component.scss'
 })
@@ -15,6 +16,22 @@ export class AddEvidenceComponent {
   
   constructor() {
 
+  }
+
+  onSubmit(form: any) {
+    console.log("Formulario")
+    console.log(form.value)
+  }
+  displayFieldPositive(form: any){
+    console.log(form.value)
+  if(!form.value.positive){
+    form.controls['positive_value'].reset();
+  }
+  }
+  displayFieldNegative(form: any){
+    if(!form.value.negative){
+      form.controls['negative_value'].reset();
+    }
   }
 
 }
