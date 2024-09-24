ec750c42f6d783733b4f163248c7d6ed0e2b6b25
AdrianCabreraPhi <adriancabproven@gmail.com>
2024-09-24 11:10:39 +0200

    combination rules added

--- src/app/select-rule/select-rule.component.ts
+++ src/app/select-rule/select-rule.component.ts
@@ -3,20 +3,32 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-select-rule',
   standalone: true,
-  imports: [MatInputModule,MatFormFieldModule,FormsModule,MatButtonModule,MatSelectModule],
+  imports: [MatInputModule,MatFormFieldModule,FormsModule,MatButtonModule,MatSelectModule,MatCheckboxModule],
   templateUrl: './select-rule.component.html',
   styleUrl: './select-rule.component.scss'
 })
-export class SelectRuleComponent {
 
+export class SelectRuleComponent {
+  ruleDisabled: boolean = false;
+  inagakiSelected: boolean = true;
 onSubmit(form:any){
   console.log("select rule")
   console.log(form.value)
+}
+
+AutoRuleSelection(form:any){
+  this.ruleDisabled = form.value.auto;
+}
+selectRule(event:MatSelectChange){
+  if(event.value == "Inagaki"){
+    this.inagakiSelected = false;
+  }
 }
 
 }
